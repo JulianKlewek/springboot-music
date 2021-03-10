eec333fc222d756ddae742d35f7f94f943a3d934
Julian Klewek <julianklewek@gmail.com>
2021-03-10 21:57:55 +0100

    Initial commit. Spotify not implemented yet

--- src/main/java/pl/klewek/springbootmusic/configuration/WebSecurityConfig.java
+++ src/main/java/pl/klewek/springbootmusic/configuration/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                    .oauth2Login()
 //                        .authorizationEndpoint()
 //                        .baseUri("/youtube/to_spotify");
+//
 
     }
 }
